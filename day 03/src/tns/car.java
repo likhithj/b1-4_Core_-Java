c8426e6426822d9fd9f6350ed914788be3ec9a19
Likhith J <Likhith J@LAPTOP-183BQES7>
2022-04-05 00:16:15 +0530

    first commit

---
+++ day 03/src/tns/car.java
@@ -0,0 +1,17 @@
+package tns;
+
+public class car 
+{
+	void display()
+	{
+		System.out.println("ROYAL CHALLENGERS BANGLORE");
+	}
+
+	public static void main(String[] args) 
+	{
+		car c=new car();
+		c.display();
+
+	}
+
+}
