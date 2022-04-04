a5aabbd5c94f069814edd163e56b2baf3b644678
Likhith J <Likhith J@LAPTOP-183BQES7>
2022-04-04 21:43:21 +0530

    first commit

---
+++ assignment/src/assignment/assignment1.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public class assignment1 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++){
+			for(int j=1;j<=i;j++){
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
