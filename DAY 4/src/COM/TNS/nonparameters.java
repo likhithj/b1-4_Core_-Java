5c60e4c43d14839a1b72b638ad4c6601f7402fc0
Likhith J <Likhith J@LAPTOP-183BQES7>
2022-04-07 09:59:39 +0530

    day 4 commit

---
+++ DAY 4/src/COM/TNS/nonparameters.java
@@ -0,0 +1,19 @@
+package COM.TNS;
+
+public class nonparameters {
+	static String str;
+	static int a;
+	static
+	{
+		 str="likhith"
+		 a=18;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(str);
+		System.out.println(a);
+		
+
+	}
+
+}
---
+++ DAY 4/src/www/tns/thiskeyword.java
@@ -0,0 +1,19 @@
+package www.tns;
+
+public class thiskeyword 
+{
+	String str;
+	thiskeyword(String str)
+	{
+		this.str=str;
+	}
+	
+
+	public static void main(String[] args) 
+	{
+		thiskeyword e=new thiskeyword("c2tc program");
+		System.out.println(e.str);
+
+	}
+
+}
