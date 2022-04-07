a25a64fec2ee923f8f2bbea352fb733ab6c9b087
Likhith J <Likhith J@LAPTOP-183BQES7>
2022-04-07 10:29:01 +0530

    DAY5 COMMIT

---
+++ day 05/src/loops/infiniteloop.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class infiniteloop {
+
+	public static void main(String[] args) 
+	{
+		int x=10;
+		while (x==10)
+		{
+			System.out.println("ROYAL CHALLENGERS BANGLORE");
+		}
+
+	}
+
+
+}
---
+++ day 05/src/loops/loops1.java
@@ -0,0 +1,19 @@
+package loops;
+
+public class loops1 
+{
+
+	public static void main(String[] args) {
+		{
+			String array[]= {"virat","gayle","ABD"};
+			for (String x:array)
+			{
+				System.out.println(x);
+			}
+				
+				
+		}
+
+	}
+
+}
