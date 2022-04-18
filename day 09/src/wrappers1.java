a9030511c7a5616bc798ab5e305350e2cf012304
Likhith J <Likhith J@LAPTOP-183BQES7>
2022-04-18 10:07:20 +0530

    day9

---
+++ day 09/src/wrappers1.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) 
+  {
+	  Integer aObj = Integer.valueOf(23);
+    Double bObj = Double.valueOf(5.55);
+    int a = aObj.intValue();
+    double b = bObj.doubleValue();
+
+    System.out.println("The value of a: " + a);
+    System.out.println("The value of b: " + b);
+  }
+}
