1f3872a9a0a4d5bc9903cce059baaaafd7de3bbd
Likhith J <Likhith J@LAPTOP-183BQES7>
2022-05-02 12:42:10 +0530

    day21

---
+++ day 21/src/annotataions/annotationas1.java
@@ -0,0 +1,16 @@
+package annotataions;
+class Animal{
+void eatSomething(){System.out.println("eating something");}
+}
+
+class Dog extends Animal{
+@Override
+void eatsomething(){System.out.println("eating foods");}//should be eatSomething
+}
+
+class TestAnnotation1{
+public static void main(String args[]){
+Animal a=new Dog();
+a.eatSomething();
+}}
+
